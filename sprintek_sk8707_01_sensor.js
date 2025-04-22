6c1646bf7184b51b25188c809e6c5b1d89f87200
Tommy Larsson <45052383+larssont@users.noreply.github.com>
2025-04-23 01:11:27 +0200

    Add silkscreen parameter to trackpoint sensor

--- sprintek_sk8707_01_sensor.js
+++ sprintek_sk8707_01_sensor.js
@@ -7,6 +7,7 @@ module.exports = {
     TPS3: { type: 'net', value: "TPS_3" },
     TPS4: { type: 'net', value: "TPS_4" },
     hole_size: 7,
+    silkscreen: true,
   },
   body: p => {
     const fp = [];
@@ -32,18 +33,19 @@ module.exports = {
     fp.push(`(pad "3" smd roundrect (at ${(flip ? -1.25 : 1.25)} 10.84 ${p.r + 90}) (size 2.4 2) (layers "${(flip ? "F" : "B")}.Cu" "${(flip ? "F" : "B")}.Paste" "${(flip ? "F" : "B")}.Mask") (roundrect_rratio 0.25) (thermal_bridge_angle 45) ${p.TPS2})`);
     fp.push(`(pad "4" smd roundrect (at ${(flip ? -3.75 : 3.75)} 10.84 ${p.r + 90}) (size 2.4 2) (layers "${(flip ? "F" : "B")}.Cu" "${(flip ? "F" : "B")}.Paste" "${(flip ? "F" : "B")}.Mask") (roundrect_rratio 0.25) (thermal_bridge_angle 45) ${p.TPS1})`);
 
-    // B.SilkS
-    fp.push(`(fp_line (start ${(flip ? 6.6 : -6.6)} -7.45) (end ${(flip ? 6.6 : -6.6)} -4.45) (stroke (width 0.1) (type default)) (layer ${(flip ? "F.SilkS" : "B.SilkS")}))`);
-    fp.push(`(fp_line (start ${(flip ? 6.6 : -6.6)} 7.84) (end ${(flip ? 6.6 : -6.6)} 10.84) (stroke (width 0.1) (type default)) (layer ${(flip ? "F.SilkS" : "B.SilkS")}))`);
-    fp.push(`(fp_line (start ${(flip ? 6.6 : -6.6)} 10.84) (end ${(flip ? 3.6 : -3.6)} 10.84) (stroke (width 0.1) (type default)) (layer ${(flip ? "F.SilkS" : "B.SilkS")}))`);
-    fp.push(`(fp_line (start ${(flip ? 3.6 : -3.6)} -7.45) (end ${(flip ? 6.6 : -6.6)} -7.45) (stroke (width 0.1) (type default)) (layer ${(flip ? "F.SilkS" : "B.SilkS")}))`);
-    fp.push(`(fp_line (start ${(flip ? -3.6 : 3.6)} 10.84) (end ${(flip ? -6.6 : 6.6)} 10.84) (stroke (width 0.1) (type default)) (layer ${(flip ? "F.SilkS" : "B.SilkS")}))`);
-    fp.push(`(fp_line (start ${(flip ? -6.6 : 6.6)} -7.45) (end ${(flip ? -3.6 : 3.6)} -7.45) (stroke (width 0.1) (type default)) (layer ${(flip ? "F.SilkS" : "B.SilkS")}))`);
-    fp.push(`(fp_line (start ${(flip ? -6.6 : 6.6)} -4.45) (end ${(flip ? -6.6 : 6.6)} -7.45) (stroke (width 0.1) (type default)) (layer ${(flip ? "F.SilkS" : "B.SilkS")}))`);
-    fp.push(`(fp_line (start ${(flip ? -6.6 : 6.6)} 10.84) (end ${(flip ? -6.6 : 6.6)} 7.84) (stroke (width 0.1) (type default)) (layer ${(flip ? "F.SilkS" : "B.SilkS")}))`);
+    // Styling
+    const displayLayer = p.silkscreen ? (flip ? "F.SilkS" : "B.SilkS") : "Dwgs.User"
 
-    fp.push(`(fp_text user "Trackpoint" (at 0 7.5 ${p.r + 0}) (unlocked yes) (layer ${(flip ? "F.SilkS" : "B.SilkS")}) (effects (font (size 1 1) (thickness 0.15)) (justify bottom${ flip ? "" : " mirror"})))`);
-    fp.push(`(fp_text user "Sensor" (at 0 9 ${p.r + 0}) (unlocked yes) (layer ${(flip ? "F.SilkS" : "B.SilkS")}) (effects (font (size 1 1) (thickness 0.15)) (justify bottom${ flip ? "" : " mirror"})))`);
+    fp.push(`(fp_line (start ${(flip ? 6.6 : -6.6)} -7.45) (end ${(flip ? 6.6 : -6.6)} -4.45) (stroke (width 0.1) (type default)) (layer ${displayLayer}))`);
+    fp.push(`(fp_line (start ${(flip ? 6.6 : -6.6)} 7.84) (end ${(flip ? 6.6 : -6.6)} 10.84) (stroke (width 0.1) (type default)) (layer ${displayLayer}))`);
+    fp.push(`(fp_line (start ${(flip ? 6.6 : -6.6)} 10.84) (end ${(flip ? 3.6 : -3.6)} 10.84) (stroke (width 0.1) (type default)) (layer ${displayLayer}))`);
+    fp.push(`(fp_line (start ${(flip ? 3.6 : -3.6)} -7.45) (end ${(flip ? 6.6 : -6.6)} -7.45) (stroke (width 0.1) (type default)) (layer ${displayLayer}))`);
+    fp.push(`(fp_line (start ${(flip ? -3.6 : 3.6)} 10.84) (end ${(flip ? -6.6 : 6.6)} 10.84) (stroke (width 0.1) (type default)) (layer ${displayLayer}))`);
+    fp.push(`(fp_line (start ${(flip ? -6.6 : 6.6)} -7.45) (end ${(flip ? -3.6 : 3.6)} -7.45) (stroke (width 0.1) (type default)) (layer ${displayLayer}))`);
+    fp.push(`(fp_line (start ${(flip ? -6.6 : 6.6)} -4.45) (end ${(flip ? -6.6 : 6.6)} -7.45) (stroke (width 0.1) (type default)) (layer ${displayLayer}))`);
+    fp.push(`(fp_line (start ${(flip ? -6.6 : 6.6)} 10.84) (end ${(flip ? -6.6 : 6.6)} 7.84) (stroke (width 0.1) (type default)) (layer ${displayLayer}))`);
+    fp.push(`(fp_text user "Trackpoint" (at 0 7.5 ${p.r + 0}) (unlocked yes) (layer ${displayLayer}) (effects (font (size 1 1) (thickness 0.15)) (justify bottom${ flip ? "" : " mirror"})))`);
+    fp.push(`(fp_text user "Sensor" (at 0 9 ${p.r + 0}) (unlocked yes) (layer ${displayLayer}) (effects (font (size 1 1) (thickness 0.15)) (justify bottom${ flip ? "" : " mirror"})))`);
 
     // Dwgs.User
     fp.push(`(fp_rect (start ${(flip ? 1.2 : -1.2)} -1.2) (end ${(flip ? -1.2 : 1.2)} 1.2) (stroke (width 0.1) (type default)) (fill solid) (layer "Dwgs.User"))`);
